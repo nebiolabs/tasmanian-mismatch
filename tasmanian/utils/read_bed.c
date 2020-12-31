a7721559118fa79c9f54d73d45d75824c87f2a00
aerijman <aerijman@fredhutch.org>
2020-12-31 14:37:13 -0500

    before fixing memory leaks

--- tasmanian/utils/read_bed.c
+++ tasmanian/utils/read_bed.c
@@ -51,8 +51,9 @@ bool isNumeric(const char *str){
 	return true;
 }
 
+void bed_to_struct(FILE *bedfile, bed_coords_t **coords, bed_data_t **data){
 
-int main(int argc, char** argv){	
+int counter=0;
 
 	char *line=NULL;
 	size_t n_characters=0, line_size=0;
@@ -60,11 +61,14 @@ int main(int argc, char** argv){
 	bed_coords_t *s, *bed_coords = NULL;
 	bed_data_t *s2, *bed_data = NULL;
 
-	while ((n_characters = getline(&line, &line_size, stdin)) != -1) {
+
+	s = (bed_coords_t *) malloc(sizeof *s);
+	s2 = (bed_data_t *) malloc(sizeof *s2);
+
+	while ((n_characters = getline(&line, &line_size, bedfile)) != -1) {
 		if (line[n_characters - 1] == '\n') {
 			line[n_characters - 1] = 0;
 		}
-		
 		// some guess for empty lines. skip them	
 		if (strlen(line) < 10) continue;
 
@@ -87,7 +91,7 @@ int main(int argc, char** argv){
 		//printf("%s\t%ld\t%ld\t%s\t%s\t%s\n",chrom, start, end, rep_name, strand, rep_class);
 
 		// if key exists in bed_coords, allocate memory for the arrays, start, stop in bed_data_.
-		s = (bed_coords_t *) malloc(sizeof *s);
+		//s = (bed_coords_t *) malloc(sizeof *s);
 		HASH_FIND_STR(bed_coords, chrom, s);
 
 		// if key is not yet in the hash table
@@ -95,7 +99,7 @@ int main(int argc, char** argv){
 			//free(s);
 			s = (bed_coords_t *) malloc(sizeof *s);
 			strcpy(s->chrom, chrom);
-			s->size = 10000;
+			s->size = 100000;
         	s->start = (unsigned long int *) malloc(s->size * sizeof s->start[0]);
         	s->end   = (unsigned long int *) malloc(s->size * sizeof s->end[0]);
 			s->counter = 0;
@@ -105,7 +109,7 @@ int main(int argc, char** argv){
 		// if we need to allocate more memory for start and end arrays
 		else {
 			if (s->size == s->counter){
-				s->size = s->size + 1000;
+				s->size = s->size + 10000;
 				s->start = realloc(s->start, s->size * sizeof (s->start[0]));
 				s->end = realloc(s->end, s->size * sizeof (s->end[0]));
 			}
@@ -126,7 +130,7 @@ int main(int argc, char** argv){
 		printf("%ld",s->start[5]);
 */
 		// FILL BED_DATA SECTION
-		s2 = (bed_data_t *) malloc(sizeof *s2);
+		//s2 = (bed_data_t *) malloc(sizeof *s2);
 		sprintf(chrom_start_end, "%s%s%ld%s%ld", chrom, "_", start,"_", end);
 		strcpy(s2->chrom_start_end, chrom_start_end);
 		strcpy(s2->rep_class_family, rep_class);
@@ -139,16 +143,35 @@ int main(int argc, char** argv){
 		HASH_FIND_STR(bed_data, chrom_start_end, s2);
 		printf("\n\n%s ********************************************\n",s2->rep_class_family);
 */
-		// FREE MEMORY SECTION
-		for  (int i=0;i<n;i++){
-			printf("\n%s, %d\n", tokens[i],i);
-			free(tokens[i]);
-		}
-		free(tokens);
-		//free(s);
-		//free(s2);
-		//printf("%s is %zu characters long", line, n_characters);
-
+counter++;
 	}
-	return(0);
+
+	// FREE MEMORY SECTION
+	for  (int i=0;i<n;i++){
+		printf("\n%d:\t%s, %d\n", counter, tokens[i],i);
+		free(tokens[i]);
+	}
+	free(tokens);
+	free(s);
+	free(s2);
+
+	*coords = bed_coords;
+	*data   = bed_data;
+}
+
+int main(int argc, char **argv) {
+	FILE *bedfile;
+	if ((bedfile = fopen(argv[1], "r")) != NULL) {
+
+		bed_coords_t *coords;
+		bed_data_t   *data;
+		bed_to_struct(bedfile, &coords, &data);
+	}
+	else {
+		fprintf(stderr, "file %s not found. Aborting!\n", argv[1]);
+		return 0;
+	}
+
+	fclose(bedfile);
+	return 0;
 }
