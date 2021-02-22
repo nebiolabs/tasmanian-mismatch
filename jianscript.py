6604cfd67fa7fb5ca6da22d38a95b9636874291b
aerijman <aerijman@m-aerijman.local>
2021-02-22 13:00:25 -0500

    reports columns as in piccard

--- jianscript.py
+++ jianscript.py
@@ -9,8 +9,13 @@ new_cols = [i.replace("N","").replace("_","->").upper() for i in cols]
 
 R1 = d.loc[d['read']==1, cols].sum(axis=0)
 
-results = np.hstack([R1[i:i+4].values / R1[i:i+4].sum() for i in [0,4,8,12]])
+alts = np.hstack([R1[i:i+4].values for i in [0,4,8,12]])
+refs = np.hstack([4*[R1[i:i+4].sum()] for i in [0,4,8,12]])
 
-for i,j in zip(new_cols, results):
-    print(i,":",j)
+results = alts/refs
+
+print("{}\t{}\t{}\t{}".format('mismatch', 'ref_count', 'alt_count','rate'))
+
+for i,j,k,l in zip(new_cols, refs, alts, results):
+    print("{}\t{}\t{}\t{:.6f}".format(i,j,k,l))
 
