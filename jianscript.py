d892934acfe12bf1226de3606ec082c49f859254
aerijman <aerijman@m-aerijman.local>
2021-02-22 14:31:04 -0500

    read2 added to jianscript

--- jianscript.py
+++ jianscript.py
@@ -8,9 +8,12 @@ cols = [i for i in d.columns if i[0]=="N"]
 new_cols = [i.replace("N","").replace("_","->").upper() for i in cols]
 
 R1 = d.loc[d['read']==1, cols].sum(axis=0)
+R2 = d.loc[d['read']==2, cols].sum(axis=0)
 
-alts = np.hstack([R1[i:i+4].values for i in [0,4,8,12]])
-refs = np.hstack([4*[R1[i:i+4].sum()] for i in [0,4,8,12]])
+R = R1+R2
+
+alts = np.hstack([R[i:i+4].values for i in [0,4,8,12]])
+refs = np.hstack([4*[R[i:i+4].sum()] for i in [0,4,8,12]])
 
 results = alts/refs
 
