a6cebd3375656061d342d649e3faa657c31d5890
aerijman <aerijman@m-aerijman.neb.com>
2021-06-07 15:21:09 -0400

    print debug WIP

--- tasmanian/tasmanian_script.py
+++ tasmanian/tasmanian_script.py
@@ -392,7 +392,8 @@ def analyze_artifacts(Input, Args):
                     
                     if PWM:
                         this_seq = ref[pos-flanking_n-1:pos+flanking_n] # Assuming 0-based index
-                        print(this_seq, Base, base)
+                        other_seq= seq[pos-flanking_n-1:pos+flanking_n]
+                        print(this_seq, Base, base, other_seq)
 
                 except Exception as e:
                     if debug:
