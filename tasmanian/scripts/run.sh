b103bd4657c73044183f339c22802c0883e31dac
aerijman <aerijman@fredhutch.org>
2025-10-23 11:58:11 -0400

    rename scripts

--- tasmanian/scripts/run.sh
+++ tasmanian/scripts/run.sh
@@ -1,5 +1,5 @@
 
-python test_runs.py
+python generate_reads.py
 bwa mem -t4 genome_simul_test simulated_reads_R1.fastq simulated_reads_R2.fastq > aligned.sam
 samtools sort --write-index aligned.sam -o aligned_sorted.bam
 samtools view aligned_sorted.bam | awk '{arr[$2]++}END{for (i in arr) {print i"\t"arr[i]} }'
